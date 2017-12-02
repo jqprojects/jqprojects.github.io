9ddbe34eb6b18f6668ee17c9e7c8161aa9e3960d
Jerry <jerryq27@gmail.com>
2017-12-02 21:33:29 +0900

    Removed unused code and implemented TypeIt on the index title.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
         window.location = "/";
     });
 
-    // $("#test").typed({
-    //     strings: ["This is a test."],
-    //     showCursor: true,
-    //     cursorChar: '_',
-    //     autoInsertCss: true
-    // });
     new TypeIt('#index-title', {
         strings: ['<b>./jqProjects</b>'],
         html: true,
