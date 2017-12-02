fb602bd4db75603873d6f87c9bcc4263d556d384
Jerry <jerryq27@gmail.com>
2017-12-02 21:22:42 +0900

    Implemented the typing animation for the index title.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function() {
     $("#title").click(function() {
         window.location = "/";
     });
+
+    // $("#test").typed({
+    //     strings: ["This is a test."],
+    //     showCursor: true,
+    //     cursorChar: '_',
+    //     autoInsertCss: true
+    // });
+    new TypeIt('#index-title', {
+        strings: ['<b>./jqProjects</b>'],
+        html: true,
+        speed: 100
+    });
 });
