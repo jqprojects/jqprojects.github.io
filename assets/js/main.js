b0181ef07517f199dbe191ce0ce25eb08ff3de63
Jerry <jerryq27@gmail.com>
2017-12-04 13:13:19 +0900

    Cleaned up animation code and implemented them in jQuery to fix the animation starting on page load.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,24 @@ $(document).ready(function() {
         window.location = "/";
     });
 
+    /******* Add fading animations. *******/
+    $("#title").hover(function() {
+        $("#title").addClass("slow-highlight");
+    });
+    $("span a").hover(function() {
+        $("span a").addClass("fast-highlight");
+    });
+    $("#about-me-icon").hover(function() {
+        $("#about-me-icon").addClass("fade");
+    });
+    $("#my-github-icon").hover(function() {
+        $("#my-github-icon").addClass("fade");
+    });
+    $("#my-projects-icon").hover(function() {
+        $("#my-projects-icon").addClass("fade");
+    });
+
+    /******* Typing animation. *******/
     new TypeIt('#index-title', {
         strings: ['<b>./jqProjects</b>'],
         html: true,
